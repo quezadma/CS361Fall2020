d5b586c2986092ec50d641f4119924e353b2ce40
maxforbang <50570544+maxforbang@users.noreply.github.com>
2020-10-30 05:47:13 -0400

    Update project4.c
    Pretty much done. Segmentation fault when you try to run ./classifier-pipes sample_clusters

--- project4.c
+++ project4.c
@@ -1,8 +1,8 @@
 /* project4.c
 
-Team:   
-Names: Martin Quezada 
-Honor code statement: 
+Team: 24
+Names: Max Forbang and Martin Quezada 
+Honor code statement: This work complies with the JMU Honor Code.
 
 */
 
@@ -27,7 +27,7 @@ Honor code statement:
 
 // This is the recommended struct for sending a message about
 // a cluster's type through the pipe. You need not use this,
-// but I would recommend that you do.
+// but I would recommend that you do. 
 struct msg {
     int msg_cluster_number;  // The cluster number from the input file
     unsigned char msg_cluster_type;  // The type of the above cluster
@@ -36,6 +36,10 @@ struct msg {
 
 int main(int argc, char *argv[])
 {
+	
+
+	char cluster_data[CLUSTER_SIZE];
+	unsigned char classification;
     int input_fd;
     int classification_fd;
     pid_t pid;
@@ -62,7 +66,6 @@ int main(int argc, char *argv[])
     }
 
 
-
     // Try opening the file for reading, exit with appropriate error message
     // if open fails
     input_fd = open(argv[1], O_RDONLY);
@@ -74,24 +77,26 @@ int main(int argc, char *argv[])
 
 
     // Create the pipe here. Exit with error if this fails.
-    if( pipe(fd_pipe) < 0)
+    if( pipe(pipefd) < 0)
     {
-        printf("Error creating pipe", strerror(errno));
+        printf("Error creating pipe: %s\n", strerror(errno));
         return 1;
     }
 
-    //method of getting the number of clusters in input file
-    int counter = 0;
-    while ((bytes_read = read(input_fd, &cluster_data, CLUSTER_SIZE)) > 0) 
-    {    
-        counter++;
-    } 
+	//getting number of clusters from size of the file
+    int fileSize, numClusters;
+	
+	fseek(input_fd, 0, SEEK_END);
+	fileSize = ftell(input_fd);
+	fseek(input_fd, 0, SEEK_SET);
+	
+	numClusters = fileSize / CLUSTER_SIZE;
 
     
 
     //creating variables to use for children logic
-    int num = counter % NUM_PROCESSES;
-    int clusters_per_child = counter / NUM_PROCESSES;    
+	int clusters_per_child = numClusters / NUM_PROCESSES;   //find how many clusters should be distributed to each child
+    int extraClusters = numClusters % NUM_PROCESSES;		//if clusters aren't evenly divisible, find how many are left over
     start_cluster = 0;
 
     // The pipe must be created at this point
@@ -102,30 +107,39 @@ int main(int argc, char *argv[])
 
     // Fork NUM_PROCESS number of child processes
     for (int i = 0; i < NUM_PROCESSES; i++) {
-
-
-        if( num != 0 )
-        {
-            clusters_to_process = clusters_per_child + 1;
-            num--;       
-        }
-
-
+		
+		//calculate start cluster before generating child
+		if (extraClusters >= i) {
+			start_cluster = i*clusters_per_child + i;
+		}
+		else if (i != 0) {
+			start_cluster = extraClusters*(clusters_per_child + 1) + (i-extraClusters)*clusters_per_child;
+		}
+		
+		//calculate number of clusters to calculate based on whether there are extra clusters or not
+		if(extraClusters > i) {
+				clusters_to_process = clusters_per_child + 1;       
+			} else {
+				clusters_to_process = clusters_per_child;
+			}
+			
         pid = fork();
         // Exit if fork() fails.
         if (pid == -1)
             exit(1);
         // This is the place to write the code for the child processes
         else if (pid == 0) {
-
-            // In this else if block, you need to implement the entire logic
+			
+			// In this else if block, you need to implement the entire logic
             // for the child processes to be aware of which clusters
             // they need to process, classify them, and create a message
             // for each cluster to be written to the pipe.
+			
+			
             
-
-
-            //message.msg_cluster_number =         
+          
+			
+                    
 
             // At this point, the child must know its start cluster and
             // the number of clusters to process.
@@ -134,25 +148,75 @@ int main(int argc, char *argv[])
               getpid(), start_cluster, clusters_to_process);
 #endif
 
-
+            close(pipefd[0]);
             // At this point the pipe must be fully set up for the child
             // This code must be executed before you start iterating over the input
             // file and before you generate and write messages.
 #ifdef GRADING // do not delete this or you will lose points
             test_pipefd(pipefd, getpid());
 #endif
-
+			
+			
 
             // Implement the main loop for the child process below this line
-            close(fd_pipe[0]);
+			
+			
+			fseek(input_fd, start_cluster * CLUSTER_SIZE, SEEK_SET);
 
-
-            for(int i =0; i < clusters_to_process; i++)
+            for(int j = 0; j < clusters_to_process; j++)
             {
-                write(fd_pipe[1], &message, sizeof(message));
+				read(input_fd, &cluster_data, CLUSTER_SIZE);
+				classification = TYPE_UNCLASSIFIED;
+        
+		
+				//Code from project 3
+				
+				//JPG CODE
+				if(has_jpg_header(cluster_data) != 0) {
+					if(has_jpg_footer(cluster_data) != 0){
+						classification = TYPE_IS_JPG | TYPE_JPG_HEADER | TYPE_JPG_FOOTER; //JHF
+					}
+					else {
+						classification = TYPE_IS_JPG | TYPE_JPG_HEADER;		//JHEADER
+					}
+				}
+				else if(has_jpg_footer(cluster_data) != 0) 
+				{
+					classification = TYPE_IS_JPG | TYPE_JPG_FOOTER;		//JFOOTER
+					
+				}
+				else if(has_jpg_body(cluster_data) != 0) {
+					classification = TYPE_IS_JPG; 		//JONLY
+				}
+
+
+				//HTML CODE
+				if(has_html_header(cluster_data) != 0) {
+					if(has_html_footer(cluster_data) != 0){
+						classification = TYPE_IS_HTML | TYPE_HTML_HEADER | TYPE_HTML_FOOTER; //HHF
+					}
+					else {
+						classification = TYPE_IS_HTML | TYPE_HTML_HEADER;		//HHEADER
+					}
+				}
+				else if(has_html_footer(cluster_data) != 0) 
+				{
+					classification = TYPE_IS_HTML | TYPE_HTML_FOOTER;		//HFOOTER
+					
+				}
+				else if(has_html_body(cluster_data) != 0) {
+					classification = TYPE_IS_HTML; 		//HONLY
+				}
+				
+				
+				//assign message correct attributes and then write message to pipe
+				struct msg currentMsg;				
+				currentMsg.msg_cluster_number = start_cluster + j;        
+				currentMsg.msg_cluster_type = classification;
+                write(pipefd[1], &currentMsg, sizeof(message));
             }
             
-            close(fd_pipe[1]);
+            close(pipefd[1]);
            
             exit(0); // This line needs to be the last one for the child
                      // process code. Do not delete this!
@@ -163,7 +227,9 @@ int main(int argc, char *argv[])
     // All the code for the parent's handling of the messages and
     // creating the classification file needs to go in the block below
 
-
+	
+	//close(pipefd[1]);
+	
     // At this point, the pipe must be fully set up for the parent
 #ifdef GRADING // do not delete this or you will lose points
     test_pipefd(pipefd, 0);
@@ -174,6 +240,10 @@ int main(int argc, char *argv[])
         // In this loop, you need to implement the processing of
         // each message sent by a child process. Based on the content,
         // a proper entry in the classification file needs to be written.
+		
+		fseek(classification_fd, message.msg_cluster_number * CLUSTER_SIZE, SEEK_SET);
+		write(classification_fd, message.msg_cluster_type, 1);
+		close(pipefd[0]);
     }
 
     close(classification_fd); // close the file descriptor for the
