88e594c76bea781588600c86ffaca622ba5b4ceb
quezadma <47606715+quezadma@users.noreply.github.com>
2020-11-03 14:43:12 -0500

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,48 @@
+//Project 3
+//Max Forbang and Martin Quezada
+//This work abides by the JMU Honor Code.
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <mqueue.h>
+
+struct msg {
+    char magic[8];
+    uint32_t id;
+    char now[200];    
+    char data[36];
+}
+
+int main (int argc, char *argv[])
+{
+    mqd_t mqd = open("/cs361lab2", O_CREAT | O_RONLY, 0600, NULL);
+
+    if(mqd < 0)
+    {
+        fprintf("MQD UNSUCCESSFUL", strerror(errno));
+        exit(1);
+    }   
+
+
+    //still need to find out where we get the arguments for the Queue output
+    fprintf("Queue %s:\n\t Flags:\t%s\n\tMax messagesses:\t%d\n\tMax size:\t%d\n\tCurrent messages:%\td");
+    
+    
+    //still need to get arguments from struct
+    fprintf("Receieved message (%d bytes):\n\tMagic: %c\n\tID: %d\n\tTime: %s\n\tData: %s");
+    
+    
+    
+    //output for invalid message
+    fprintf("Receieved message (%d bytes):\n\tInvalid message");
+
+    
+    mq_close(mqd);
+}
+
